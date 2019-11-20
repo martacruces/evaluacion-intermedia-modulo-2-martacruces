8e01f01a7d9f304c4fcdd7ed50c5914a3ff49f81
martacruces <martacrucesdiaz@gmail.com>
2019-11-20 17:01:20 +0100

    conditional clue added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 'use strict';
 
 const elementButton = document.querySelector('#button-default');
-const elementInput = document.querySelector('#inputcell');
-const elementClue= document.querySelector('#clue');
-const attempts= document.querySelector('#attempts');
+const elementInput = document.querySelector('#wonderingnumber');
+const elementClue = document.querySelector('#clue');
+const elementAttempts = document.querySelector('#attempts');
 
 const getRandomNumber = max => Math.ceil(Math.random() * max);
 
 function playGame(){
-    getRandomNumber(100)
-    console.log(getRandomNumber(100));
-    
-}
+    const randomNumber = getRandomNumber(100)
+    console.log(randomNumber);
+
+    if (elementInput.value > randomNumber) {
+        elementClue.innerHTML = 'Demasiado alto';
+    } else if(elementInput.value < randomNumber) {
+        elementClue.innerHTML = 'Demasiado bajo'
+    } else if(elementInput.value = randomNumber){
+        elementClue.innerHTML = '¡Has ganado, campeona!'
+    }
+
+};
 
 elementButton.addEventListener('click', playGame);
