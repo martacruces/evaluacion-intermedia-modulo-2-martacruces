16f6d85b8d33e32a46d0e05c9b0a4b800e9fd222
martacruces <martacrucesdiaz@gmail.com>
2019-11-20 16:26:56 +0100

    adding random number function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const elementButton = document.querySelector('#button-default');
+const elementInput = document.querySelector('#inputcell');
+const elementClue= document.querySelector('#clue');
+const attempts= document.querySelector('#attempts');
+
+const getRandomNumber = max => Math.ceil(Math.random() * max);
+
+function playGame(){
+    getRandomNumber(100)
+    console.log(getRandomNumber(100));
+    
+}
+
+elementButton.addEventListener('click', playGame);
