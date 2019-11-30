ac175af5c24b2949557f09c5c40a02a32f839ba2
martacruces <martacrucesdiaz@gmail.com>
2019-11-30 20:00:21 +0100

    Issue review

--- js/main.js
+++ js/main.js
@@ -3,24 +3,37 @@
 const elementButton = document.querySelector('#button-default');
 const elementInput = document.querySelector('#wonderingnumber');
 const elementClue = document.querySelector('#clue');
-const elementAttempts = document.querySelector('#attempts');
+const elementAttemps = document.querySelector('#attemps');
+
+const maxNumber = 100;
+
+let attemps = 0;
+elementAttemps.innerHTML = attemps;
 
 const getRandomNumber = max => Math.ceil(Math.random() * max);
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
 
-
-function playGame(){
-    const randomNumber = getRandomNumber(100)
-    console.log(randomNumber);
-
-    if (elementInput.value > randomNumber) {
-        elementClue.innerHTML = 'Demasiado alto';
-    } else if(elementInput.value < randomNumber) {
-        elementClue.innerHTML = 'Demasiado bajo'
-    } else if(elementInput.value = randomNumber){
-        elementClue.innerHTML = '¡Has ganado, campeona!'
-    }
-
+const increaseAttemps = () => {
+    attemps++;
+    elementAttemps.innerHTML = attemps;
 };
 
-window.addEventListener('load', randomNumber)
+const checkUserNumber = () => {
+    if(elementInput.value > maxNumber || elementInput.value === '' || elementInput.value <= 0){
+        elementClue.innerHTML = 'El número tiene que estar entre 1 y 100'
+    } else if (elementInput.value > randomNumber){
+        elementClue.innerHTML = 'El número es demasiado alto';
+    } else if (elementInput.value < randomNumber) {
+        elementClue.innerHTML = 'El número es demasiado bajo';
+    } else {
+        elementClue.innerHTML = '¡HAS GANADO!';
+    }
+};
+
+function playGame(){
+    increaseAttemps();
+    checkUserNumber();
+};
+
 elementButton.addEventListener('click', playGame);
